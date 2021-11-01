eabd7a5a7e9876fbac6ceb38c8420c38985b96d8
GLBryan <garrettleebryan@gmail.com>
2021-11-01 11:43:19 -0400

    coded createManager()

--- index.js
+++ index.js
@@ -1,51 +1,46 @@
 const fs = require('fs')
 const inq = require('inquirer')
 
-const managerQuestions = [
-    {
-        type: "input",
-        message: "Enter Team Manager's name.",
-        name: "name",
-    },
-    {
-        type: "input",
-        message: "Enter Team Manager's ID number.",
-        name: "ID",
-    },
-    {
-        type: "input",
-        message: "Enter Team Manager's email address.",
-        name: "email",
-    },
-    {
-        type: "input",
-        message: "Enter Team Manager's office phone number.",
-        name: "phoneNumber",
-    },   
-]
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Manager = require('./lib/Manager')
+const Intern = require('./lib/Intern')
+
+const team = []
 
 function createManager() {
-    inq.prompt(managerQuestions).then(data => {
-        const {name, ID, email, phoneNumber} = data
+    inq
+        .prompt([
+            {
+                type: "input",
+                message: "Enter Team Manager's name.",
+                name: "name",
+            },
+            {
+                type: "input",
+                message: "Enter Team Manager's ID number.",
+                name: "ID",
+            },
+            {
+                type: "input",
+                message: "Enter Team Manager's email address.",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "Enter Team Manager's office phone number.",
+                name: "phoneNumber",
+            },   
+        ])
+    
+        .then(data => {
+            const {name, ID, email, phoneNumber} = data
 
-        addEmployee(name, ID, email, phoneNumber)
-    })
+            const manager = new Manager(name, ID, email, phoneNumber)
+            team.push(manager)
+            console.log(team)
+            console.log(manager)
+        })
 }
 
-function createEmployee() {
-    again = true
-
-    const employeeType = [
-        {
-            type: "list",
-            message: "What type of employee do you want to add?",
-            name: "name",
-            choices: ["Engineer", "Intern", "None"],
-        },
-    ]
-
-    while (again) {
-        inq.prompt().then()
-    }
-
-}
+createManager()
